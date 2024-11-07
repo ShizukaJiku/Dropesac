5cc6d16e471ee1e8673361294a60ae9e356b58f0
shizu <shizuka.jiku@gmail.com>
2024-11-07 15:57:21 -0500

    Fix more layouts

--- app/src/main/java/com/dropesac/app/PresentationActivity.kt
+++ app/src/main/java/com/dropesac/app/PresentationActivity.kt
@@ -1,33 +1,20 @@
 package com.dropesac.app
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class PresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // Configurar ajustes de insets (bordes) de la pantalla
+        setContentView(R.layout.activity_presentation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Encuentra el botón de login
-        val loginButton: Button = findViewById(R.id.login_button)
-
-        // Configura el botón para iniciar PresentationActivity al hacer clic
-        loginButton.setOnClickListener {
-            val intent = Intent(this, PresentationActivity::class.java)
-            startActivity(intent)
         }
     }
 }
